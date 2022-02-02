79c0b0ce9e28b6be57dce9feafe7a6908d610f71
Benjamin Schleiss <benjamin@kauz.ch>
2022-02-02 14:58:00 +0100

    Copied template

---
+++ Uebung1_2/data.js
@@ -0,0 +1,15 @@
+export default {
+    addresses: [{
+            id: 1,
+            firstname: "Hans",
+            lastname: "Muster",
+            city: "Zürich",
+        },
+        {
+            id: 2,
+            firstname: "John",
+            lastname: "Doe",
+            city: "Zürich",
+        },
+    ],
+};
