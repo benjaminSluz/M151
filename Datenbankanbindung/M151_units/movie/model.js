01a15f1fba6714b73fb7ee272cb3d1a20b1886e4
Benjamin Schleiss <benjamin@kauz.ch>
2022-03-23 15:02:18 +0100

    Created Datenabnkanbindung for mysql

---
+++ Datenbankanbindung/M151_units/movie/model.js
@@ -0,0 +1,31 @@
+import mysql from "mysql2/promise";
+
+const connection = await mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "sml12345",
+    database: "movie-db",
+});
+await connection.connect();
+
+export async function getAll() {
+    const query = "SELECT * FROM Movies";
+    const [data] = await connection.query(query);
+    return data;
+}
+
+async function insert(movie) {
+    TODO;
+}
+async function update(movie) {
+    TODO;
+}
+export async function get(id) {
+    TODO;
+}
+export async function remove(id) {
+    TODO;
+}
+export function save(movie) {
+    TODO;
+}
