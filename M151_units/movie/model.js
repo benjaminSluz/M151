f712699abb4c7cac5b2f4240558a3d91c6aa4aaa
Benjamin Schleiss <benjamin@kauz.ch>
2022-04-27 09:52:37 +0200

    Added Mongodb as database

--- M151_units/movie/model.js
+++ M151_units/movie/model.js
@@ -1,39 +1,43 @@
-let data = [
-    { id: 1, title: "Avengers: Endgame", year: "2019" },
-    { id: 2, title: "Avengers: Infinity War", year: "2018" },
-    { id: 3, title: "Captain America: The First Avenger", year: "2011" },
-    { id: 3, title: "Spider-Man: Far From Home", year: "2019" },
-];
+import { MongoClient } from "mongodb";
 
-function getNextId() {
-    return Math.max(...data.map((movie) => movie.id)) + 1;
-}
+let collection = null;
 
-function insert(movie) {
-    movie.id = getNextId();
-    data.push(movie);
-}
+async function connect() {
+    if (collection) return collection;
+    const client = new MongoClient("mongodb://localhost:27017");
+    await client.connect();
 
-function update(movie) {
-    movie.id = parseInt(movie.id, 10);
-    const index = data.findIndex((item) => item.id === movie.id);
-    data[index] = movie;
+    const db = client.db("moviedb");
+    collection = db.collection("Movie");
+
+    return collection;
 }
-export function getAll() {
-    return Promise.resolve(data);
+export async function getAll() {
+    const collection = await connect();
+    const docs = await collection.find({});
+    return docs.toArray();
 }
-export function get(id) {
-    return Promise.resolve(data.find((movie) => movie.id === id));
+export async function get(id) {
+    const collection = await connect();
+    const docs = await collection.findOne(id);
+    return docs.toArray();
 }
-export function remove(id) {
-    data = data.filter((movie) => movie.id !== id);
-    return Promise.resolve();
+export async function remove(id) {
+    //  TODO;
+}
+async function insert(movie) {
+    movie.id = Date.now();
+    const collection = await connect();
+    const data = collection.insertOne(movie);
+    return data;
+}
+async function update(movie) {
+    //  TODO;
 }
 export function save(movie) {
-    if (movie.id === "") {
-        insert(movie);
+    if (!movie.id) {
+        return insert(movie);
     } else {
-        update(movie);
+        return update(movie);
     }
-    return Promise.resolve();
 }
