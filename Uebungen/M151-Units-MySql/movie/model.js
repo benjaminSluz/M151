42bf07cabacb8c8060027c40d4cd8b3b999126fa
Benjamin Schleiss <benjamin@kauz.ch>
2022-05-17 16:41:44 +0200

    changed connection to root

--- Uebungen/M151-Units-MySql/movie/model.js
+++ Uebungen/M151-Units-MySql/movie/model.js
@@ -1,49 +1,49 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
 
 const connection = await mysql.createConnection({
-  host: '127.0.0.1',
-  //port: 3307,
-  user: 'vmadmin',
-  password: 'sml12345',
-  database: 'movie-db',
+    host: "127.0.0.1",
+    //port: 3307,
+    user: "root",
+    password: "",
+    database: "movie-db",
 });
 
 await connection.connect();
 
 export async function getAll() {
-  const query = 'SELECT * FROM Movies';
-  const [data] = await connection.query(query);
-  return data;
+    const query = "SELECT * FROM Movies";
+    const [data] = await connection.query(query);
+    return data;
 }
 
 async function insert(movie) {
-  const query = 'INSERT INTO Movies (title, year) VALUES (?, ?)';
-  const [result] = await connection.query(query, [movie.title, movie.year]);
-  return { ...movie, id: result.insertId };
+    const query = "INSERT INTO Movies (title, year) VALUES (?, ?)";
+    const [result] = await connection.query(query, [movie.title, movie.year]);
+    return {...movie, id: result.insertId };
 }
 
 async function update(movie) {
-  const query = 'UPDATE Movies SET title = ?, year = ? WHERE id = ?';
-  await connection.query(query, [movie.title, movie.year, movie.id]);
-  return movie;
+    const query = "UPDATE Movies SET title = ?, year = ? WHERE id = ?";
+    await connection.query(query, [movie.title, movie.year, movie.id]);
+    return movie;
 }
 
 export async function get(id) {
-  const query = 'SELECT * FROM Movies WHERE id = ?';
-  const [data] = await connection.query(query, [id]);
-  return data.pop();
+    const query = "SELECT * FROM Movies WHERE id = ?";
+    const [data] = await connection.query(query, [id]);
+    return data.pop();
 }
 
 export async function remove(id) {
-  const query = 'DELETE FROM Movies WHERE id = ?';
-  await connection.query(query, [id]);
-  return;
+    const query = "DELETE FROM Movies WHERE id = ?";
+    await connection.query(query, [id]);
+    return;
 }
 
 export function save(movie) {
-  if (!movie.id) {
-    return insert(movie);
-  } else {
-    return update(movie);
-  }
+    if (!movie.id) {
+        return insert(movie);
+    } else {
+        return update(movie);
+    }
 }
